7b4f02f592e81d15498fc5261dfe25c03017864b
Mwisya Julius <fm@gmail.com>
2025-03-15 17:18:46 +0300

    routes fix

--- src/index.js
+++ src/index.js
@@ -4,18 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { RoomsProvider } from './contextApi/roomsContext';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter basename="/brandhotel">
+    <BrowserRouter basename="/brandhotel">
       <RoomsProvider>
         <App/>
       </RoomsProvider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
